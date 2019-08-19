e49cb53fe80c54be21e58b464484b2a815f8b023
Joaco <joaquin.abeiro@gmail.com>
2019-08-18 22:28:49 -0300

    Parse update

--- EvTec.rb
+++ EvTec.rb
@@ -7,19 +7,20 @@ p = gets
 puts "Well I don't care what port you want, I'm going to put it in 2020 because I can"
 PORT = 2020
 socket = TCPServer.new('localhost', PORT)
-def handle_connection(client)
-  puts "Good Morning! #{client}"
-  client.write("Good Afternoon")
-  client.close
+
+puts "Ctrl + c to stop"
+loop {
+  client  = server.accept
+  request = client.readpartial(2048)
+  puts request
+}
+
+def parse(request)
+  method, path, version = request.lines[0].split
+  {
+    path: path,
+    method: method,
+    headers: version,
+  }
 end
-
-puts "Listening on #{PORT}. Press CTRL+C to cancel."
-loop do
-  client = socket.accept
-  Thread.new { handle_connection(client) }
-end
-
-#-falta implementar memcached-#
-
-
 
